cf69c25fcaa9c9b61394f9178c4ecaaacbd95a41
sakaaaaaaa <tugassakaa@gmail.com>
2025-05-15 14:55:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🙈sakaageraldine")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "akuu saka aku bangga jadi anak mamah"
 )
